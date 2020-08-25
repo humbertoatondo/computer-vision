f76b2b087b3c0742a0169e84ac8f3da84bd57e94
Humberto Atondo <humberto_atondo@hotmail.com>
2020-08-24 20:50:33 -0500

    done

--- practice/1.1/calibrate.py
+++ practice/1.1/calibrate.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import cv2
 import glob
@@ -18,66 +17,68 @@ img_points = [] # 2d points in image plane.
 
 images = glob.glob(r'images/*.jpeg')
 
-found = 0
+# Create new directory for image results in calibration.
 path = os.getcwd()
 results_dir = path + "/calibration_results"
 if not os.path.exists(results_dir):
     os.mkdir(results_dir)
 
-for fname in images:  # Here, 10 can be changed to whatever number you like to choose
-    img = cv2.imread(fname) # Capture frame-by-frame
-    #print(images[im_i])
+found = 0
+for fname in images:
+    img = cv2.imread(fname)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
     # Find the chess board corners
     ret, corners = cv2.findChessboardCorners(gray, (7,7), None)
+
     # If found, add object points, image points (after refining them)
     if ret == True:
-        obj_points.append(objp)   # Certainly, every loop objp is the same, in 3D.
+        obj_points.append(objp)
+
         corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
         img_points.append(corners2)
+
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, (7,7), corners2, ret)
-        found += 1
         cv2.imshow('img', img)
         cv2.waitKey(500)
-        # if you want to save images with detected corners 
-        # uncomment following 2 lines and lines 5, 18 and 19
-        image_name = path + '/results/result' + str(found) + '.png'
+        
+        # Save results from calibration.
+        image_name = results_dir + '/result' + str(found) + '.png'
         cv2.imwrite(image_name, img)
 
-print("Number of images used for calibration: ", found)
+        found += 1
 
-# When everything done, release the capture
-# cap.release()
+print("Number of images used for calibration: ", found)
 cv2.destroyAllWindows()
 
-# calibration
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
-
-# transform the matrix and distortion coefficients to writable lists
-data = {'camera_matrix': np.asarray(mtx).tolist(),
-        'dist_coeff': np.asarray(dist).tolist()}
-
-# and save it to a file
-with open("calibration_matrix.yaml", "w") as f:
-    yaml.dump(data, f)
-
-
-
-
-
-
-
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1],None,None)
 
 img = cv2.imread('test.jpeg')
 h,  w = img.shape[:2]
 newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
+
+
+
+
 # undistort
 mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
 
 # crop the image
 x,y,w,h = roi
 dst = dst[y:y+h, x:x+w]
 cv2.imwrite('calibresult.png',dst)
+
+
+
+
+# Re-projection Error
+mean_error = 0
+for i in range(len(obj_points)):
+    img_points2, _ = cv2.projectPoints(obj_points[i], rvecs[i], tvecs[i], mtx, dist)
+    error = cv2.norm(img_points[i],img_points2, cv2.NORM_L2)/len(img_points2)
+    mean_error += error
+
+print("total error: ", mean_error/len(obj_points))
