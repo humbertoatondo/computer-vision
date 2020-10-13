76348e8e9b107b2264c851deac729f34c3ffe93b
Humberto Atondo <humberto_atondo@hotmail.com>
2020-10-12 20:50:33 -0500

    Practice 2.3

--- practice/2.3/main.py
+++ practice/2.3/main.py
@@ -8,17 +8,27 @@ def main():
     while True:
         ret, img = cap.read()
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, thresh = cv2.threshold(
-            gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        blur = cv2.GaussianBlur(gray, (7, 7), 0)
 
-        kernel = np.ones((3, 3), np.uint8)
-        eroded = cv2.erode(thresh, kernel, iterations=3)
-        dilated = cv2.dilate(eroded, kernel, iterations=2)
+        # originalImage = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        originalImage = cv2.cvtColor(blur, cv2.COLOR_GRAY2RGB)
+        reshapedImage = np.float32(originalImage.reshape(-1, 3))
 
-        cv2.imshow("Video", img)
-        cv2.imshow("Video", thresh)
-        cv2.imshow("Video Binary", dilated)
+        numberOfClusters = 6
+
+        stopCriteria = (cv2.TERM_CRITERIA_MAX_ITER, 5, 0.1)
+
+        ret, labels, clusters = cv2.kmeans(
+            reshapedImage, numberOfClusters, None, stopCriteria, 2, cv2.KMEANS_RANDOM_CENTERS)
+
+        clusters = np.uint8(clusters)
+
+        intermediateImage = clusters[labels.flatten()]
+        clusteredImage = intermediateImage.reshape((originalImage.shape))
+
+        cv2.imshow("Video", originalImage)
+        cv2.imshow("K-Means", clusteredImage)
 
         if cv2.waitKey(1) == 27:
             break
