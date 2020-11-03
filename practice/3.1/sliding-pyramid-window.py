06630ff37059e849cdc9812847869dcac6873b44
Humberto Atondo <humberto_atondo@hotmail.com>
2020-11-02 19:48:28 -0600

    sliding pyramid window

---
+++ practice/3.1/sliding-pyramid-window.py
@@ -0,0 +1,39 @@
+import imutils
+import argparse
+import time
+import cv2
+
+
+def pyramid(image, scale=1.5, minSize=(30, 30)):
+    yield image
+    while True:
+        w = int(image.shape[1] / scale)
+        image = imutils.resize(image, width=w)
+        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+            break
+        yield image
+
+
+def sliding_window(image, stepSize, windowSize):
+    for y in range(0, image.shape[0], stepSize):
+        for x in range(0, image.shape[1], stepSize):
+            yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
+
+
+def main():
+    image = cv2.imread("face.jpg")
+    (winW, winH) = (128, 128)
+
+    for resized in pyramid(image, scale=1.5):
+        for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
+            if window.shape[0] != winH or window.shape[1] != winW:
+                continue
+            clone = resized.copy()
+            cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+            cv2.imshow("Window", clone)
+            cv2.waitKey(1)
+            time.sleep(0.025)
+
+
+if __name__ == "__main__":
+    main()
